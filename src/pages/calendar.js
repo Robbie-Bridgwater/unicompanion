f4fa055c6eb018b6566ccb12cd31a22fe6c2e64d
Robbie Bridgwater <Robbie_Bridgwater@Hotmail.co.uk>
2021-05-29 10:47:03 +0100

    added the calendar component

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -1,10 +1,26 @@
 import React from "react";
-import Wrapper from "../components/Wrapper";
+import Wrapper from "../components/Wrapper"
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
-const Calendar = () => {
-    return(
-        <Wrapper></Wrapper>
+const localizer = momentLocalizer(moment)
+const myEventsList = [];
+
+const CalendarPage = () => {
+    return (
+        <Wrapper>
+
+            <Calendar
+                localizer={localizer}
+                events={myEventsList}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 500 }}
+            />
+
+        </Wrapper>
     )
 }
 
-export default Calendar
+export default CalendarPage
