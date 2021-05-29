2583129ea0b8c2337eba58d447291c49cb8d4e05
Matthew Jones <mejones92@gmail.com>
2021-05-29 17:32:47 +0100

    Updated file paths for api routes.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,14 +1,12 @@
 const path = require("path");
 const router = require("express").Router();
-const calendarRoutes = require("./calendar");
-// REQUIRE IN API ROUTES HERE
+const calendarRoutes = require("./calendar.js");
 
 router.use("/calendar", calendarRoutes);
 
-
 // For anything else, render the html page CHANGE TO BUILD ON DEPLOY
 router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../../client/public/index.html"));
+  res.sendFile(path.join(__dirname, "../../public/index.html"));
 });
 
 module.exports = router;
