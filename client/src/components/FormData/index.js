e6a923fa635e9ee932e0dc83be58f6db9e7e8e50
George Cope <G.Cope13@gmail.com>
2021-06-01 20:26:31 +0100

    Added some hardcode dummy data for sports & societies

---
+++ client/src/components/FormData/index.js
@@ -0,0 +1,48 @@
+// const sports = ['Hockey', 'Rugby', 'Football', 'Lacrosse'];
+// const societies = ['Film', 'Music', 'Dance', 'Chess'];
+
+export const sports = [
+    {
+        id: 1,
+        type: 'Football',
+        description: 'Kick a ball!'
+    },
+    {
+        id: 2,
+        type: 'Hockey',
+        description: 'Play with sticks!'
+    },
+    {
+        id: 3,
+        type: 'Rugby',
+        description: 'Hold a ball!'
+    },
+    {
+        id: 4,
+        type: 'Lacrosse',
+        description: 'We don\'t know what\'s going on!'
+    }
+]
+
+export const societies = [
+    {
+        id: 1,
+        type: 'Film',
+        description: 'We like movies!'
+    },
+    {
+        id: 2,
+        type: 'Dance',
+        description: 'We like dancing!'
+    },
+    {
+        id: 3,
+        type: 'Music',
+        description: 'We\'re pretentious!'
+    },
+    {
+        id: 4,
+        type: 'Drinking',
+        description: 'Spend all your student finance!'
+    }
+]
