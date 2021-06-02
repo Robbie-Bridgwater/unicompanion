0827e49067a91be8ef23896780d21be27438993e
Matthew Jones <mejones92@gmail.com>
2021-06-02 18:08:28 +0100

    Added controller for User model.

---
+++ controllers/userController.js
@@ -0,0 +1,30 @@
+const User = require("../models/User.js");
+
+module.exports = {
+  findAll: function(req, res) {
+    User.find(req.query)
+      .then(dbUser => res.json(dbUser))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    User.findById(req.params.id)
+      .then(dbUser => res.json(dbUser))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    User.create(req.body)
+      .then(dbUser => res.json(dbUser))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    User.findOneAndUpdate({ id: req.params.id }, req.body)
+      .then(dbUser => res.json(dbUser))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    User.findById(req.params.id)
+      .then(dbUser => dbUser.remove())
+      .then(dbUser => res.json(dbUser))
+      .catch(err => res.status(422).json(err));
+  }
+};
