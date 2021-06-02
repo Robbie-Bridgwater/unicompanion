4a6b385415fcd49c5f6fc4c38b6b850eb48aee43
Ben <pauleybj@gmail.com>
2021-06-02 20:40:59 +0100

    implemented login front end

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -1,10 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import Wrapper from "../components/Wrapper";
+import LoginForm from "../components/LoginForm";
+import "./Account.css";
 
 const Account = () => {
-    return(
-        <Wrapper></Wrapper>
-    )
-}
+  const history = useHistory();
 
-export default Account
+  const adminUser = {
+    email: "admin@email.com",
+    password: "admin123",
+  };
+
+  const [user, setUser] = useState({ name: "", email: "" });
+  const [error, setError] = useState("");
+
+  const Login = (details) => {
+    console.log(details);
+
+    if (
+      details.email === adminUser.email &&
+      details.password === adminUser.password
+    ) {
+      setUser({
+        name: details.name,
+        email: details.email,
+      });
+    } else {
+      setError("Details do not match!");
+    }
+  };
+
+  // const Logout = () => {
+  //   setUser({ name: "", email: "" });
+  // };
+
+  const reroute = () => {
+    history.push("");
+  };
+
+  return (
+    <Wrapper>
+      <div id="account">
+        {user.email !== "" ? (
+          reroute()
+        ) : (
+          <LoginForm Login={Login} error={error} />
+        )}
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Account;
