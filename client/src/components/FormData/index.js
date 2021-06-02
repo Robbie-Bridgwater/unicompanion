a2303b9ec96fab68237ebf6596ec2abfbb2fd144
George Cope <G.Cope13@gmail.com>
2021-06-02 20:59:39 +0100

    Added an email field to each of the objects

--- client/src/components/FormData/index.js
+++ client/src/components/FormData/index.js
@@ -1,26 +1,27 @@
-// const sports = ['Hockey', 'Rugby', 'Football', 'Lacrosse'];
-// const societies = ['Film', 'Music', 'Dance', 'Chess'];
-
 export const sports = [
     {
         id: 1,
         type: 'Football',
-        description: 'Kick a ball!'
+        description: 'Kick a ball!',
+        email: 'Football@bham.ac.uk'
     },
     {
         id: 2,
         type: 'Hockey',
-        description: 'Play with sticks!'
+        description: 'Play with sticks!',
+        email: 'Hockey@bham.ac.uk'
     },
     {
         id: 3,
         type: 'Rugby',
-        description: 'Hold a ball!'
+        description: 'Hold a ball!',
+        email: 'Rugby@bham.ac.uk'
     },
     {
         id: 4,
         type: 'Lacrosse',
-        description: 'We don\'t know what\'s going on!'
+        description: 'We don\'t know what\'s going on!',
+        email: 'Lacrosse@bham.ac.uk'
     }
 ]
 
@@ -28,21 +29,25 @@ export const societies = [
     {
         id: 1,
         type: 'Film',
-        description: 'We like movies!'
+        description: 'We like movies!',
+        email: 'Film@bham.ac.uk'
     },
     {
         id: 2,
         type: 'Dance',
-        description: 'We like dancing!'
+        description: 'We like dancing!',
+        email: 'Dance@bham.ac.uk'
     },
     {
         id: 3,
         type: 'Music',
-        description: 'We\'re pretentious!'
+        description: 'We\'re pretentious!',
+        email: 'Music@bham.ac.uk'
     },
     {
         id: 4,
         type: 'Drinking',
-        description: 'Spend all your student finance!'
+        description: 'Spend all your student finance!',
+        email: 'Drinking@bham.ac.uk'
     }
 ]
