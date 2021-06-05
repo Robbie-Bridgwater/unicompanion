b945005edc3c58f0dab4fcc7e0039645a1886d5d
Gurvinder Dehl <gurvinder360@hotmail.com>
2021-06-05 09:44:30 +0100

    Header completed

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header(props) {
+    return (
+        <div>
+            <h1>{props.title}</h1>
+
+            <p>Hello</p>
+        
+        </div>
+    )
+
+}
+export default Header;
