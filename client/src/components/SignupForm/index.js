30523ed57a5eb967a35f73cb7fff6721b62c44fc
Ben <pauleybj@gmail.com>
2021-06-05 09:34:05 +0100

    implemented non-functional sign up form front-end

---
+++ client/src/components/SignupForm/index.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+
+function SignUpForm({ Signup, error }) {
+  const [details, setDetails] = useState({ name: "", email: "", password: "" });
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    Signup(details);
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="form-inner">
+        <h2>Sign Up</h2>
+        <div className="form-group">
+          <label htmlFor="name">Name: </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            onChange={(e) => setDetails({ ...details, name: e.target.value })}
+            value={details.name}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email: </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            onChange={(e) => setDetails({ ...details, email: e.target.value })}
+            value={details.email}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password: </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={(e) =>
+              setDetails({ ...details, password: e.target.value })
+            }
+            value={details.password}
+          />
+        </div>
+        {error !== "" ? <div className="error">{error}</div> : ""}
+        <br />
+        <input type="submit" value="SIGNUP" />
+      </div>
+    </form>
+  );
+}
+
+export default SignUpForm;
