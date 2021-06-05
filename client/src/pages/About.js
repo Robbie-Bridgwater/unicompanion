048d70644adbe65bd553458cb4e6b6be5de4e60a
Gurvinder Dehl <gurvinder360@hotmail.com>
2021-06-05 09:45:49 +0100

    adjusting the page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -3,3 +3,14 @@ import Wrapper from "../components/Wrapper";
 import ContactForm from "../components/ContactForm";
 import Header from "../components/Header ";
 import Image1 from "../assets/img/birmingham_university.jpg";
+
+
+const About = (props) => {
+    return (
+
+        <Wrapper>
+            <div className="row">
+                <div className="col-8">
+                    <img src={Image1}
+                    width ="100%" className="img=responsive"></img>
+                    <Header title="About Us" />
