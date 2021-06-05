a8e7a217a5b2876b596366fa1345c4d3d71e6359
Gurvinder Dehl <gurvinder360@hotmail.com>
2021-06-05 09:41:48 +0100

    editing contact form

--- client/src/components/ContactForm/index.js
+++ client/src/components/ContactForm/index.js
@@ -2,4 +2,11 @@ const Contact = () => {
 
     return (
 
-    
+        <div>
+        <form>
+            <div className="container">
+                <div className="mb-3">
+                <div id="emailHelp" className="form-text"><h1>Contact Us</h1></div>
+                
+                <div id="emailHelp" class="form-text">If you have any further questions please don't hesistate to get in touch below!</div>
+                <br></br>
