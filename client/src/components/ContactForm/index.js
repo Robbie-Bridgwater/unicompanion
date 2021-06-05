2faa5b014e3ed6896b367c665ca3e8e4c1e2d13a
Gurvinder Dehl <gurvinder360@hotmail.com>
2021-06-05 14:23:34 +0100

    styling

--- client/src/components/ContactForm/index.js
+++ client/src/components/ContactForm/index.js
@@ -1,17 +1,18 @@
 import React from "react"
+import "./style.css";
 
 const Contact = () => {
     return (
 
-        <div className= "border">
-        <form>
-            
-            <div className="container">
-                <div className="fluid-mb-3">
-                <div id="emailHelp" className="form-text"><h1>Contact Us</h1></div>
-                
-                <div id="emailHelp" class="form-text">If you have any further questions please get in touch below!</div>
-                <br></br>
+        <div>
+            <form>
+                <div className="border">
+             
+                    <div className="fluid-mb-3">
+                        <div id="emailHelp" className="form-text"><h1>Contact Us</h1></div>
+
+                        <div id="emailHelp" class="form-text">If you have any further questions please get in touch below!</div>
+                        <br></br>
                         <label for="exampleFullName" className="form-label">Full Name</label>
                         <input type="text" className="form-control" id="exampleName" aria-describedby="fullName" />
                     </div>
