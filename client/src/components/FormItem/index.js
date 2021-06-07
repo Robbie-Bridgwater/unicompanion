ae079bce98892513a7824ad518bc109f256a4261
George Cope <G.Cope13@gmail.com>
2021-06-07 19:20:02 +0100

    Removed unecessary & moved it to Form component. Also, altered which props are required

--- client/src/components/FormItem/index.js
+++ client/src/components/FormItem/index.js
@@ -1,16 +1,12 @@
-import { React, useState } from 'react';
+import { React } from 'react';
 import './style.css';
 
 export const FormItem = (props) => {
-    const [isChecked, setChecked] = useState(false);
-    const handleChange = () => {
-        setChecked(!isChecked);
-    };
 
     return (
         <div className='formItem'>
-            <input type='checkbox' id={ props.id } name={ props.type } value={ props.type } checked={isChecked} onChange={handleChange}></input>
-            <label htmlFor={ props.id }> { props.type } </label>
+            <input type='checkbox' id={ props.id } name={ props.type } value={ props.name } checked={ props.checked } onChange={ props.handleChange }></input>
+            <label htmlFor={ props.id }> { props.name } </label>
             <p>Description: <br></br> { props.description }</p>
             <p>Contact: <a href={ `mailto: ${ props.email }` }> { props.email }</a> </p>  
         </div>
