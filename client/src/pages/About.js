3262e6556519f3fb47a1ea6334c02e5817b95760
Ben <pauleybj@gmail.com>
2021-06-08 16:30:21 +0100

    renamed Header component to AboutUs

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,29 +1,25 @@
 import React from "react";
 import Wrapper from "../components/Wrapper";
 import ContactForm from "../components/ContactForm";
-import Header from "../components/Header";
-import ParallaxImage from '../components/Parallax';
-
+import AboutUs from "../components/AboutUs";
+import ParallaxImage from "../components/Parallax";
 
 const About = (props) => {
-    return (
-        
-        <Wrapper>
-            <ParallaxImage />
-            
-            <div className="row">
-                <div className="col-8">
-                    <Header title="About Us" />
-                     <p></p>
-                </div>
-                <div className="col-4">
-                    <ContactForm />
-                </div>
-            </div>
+  return (
+    <Wrapper>
+      <ParallaxImage />
 
+      <div className="row">
+        <div className="col-8">
+          <AboutUs title="About Us" />
+          <p></p>
+        </div>
+        <div className="col-4">
+          <ContactForm />
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 
-        </Wrapper>
-    )
-}
-
-export default About
+export default About;
