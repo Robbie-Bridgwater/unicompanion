6eb384d40aea07de7eef5024df785538b756ddcf
Ben <pauleybj@gmail.com>
2021-06-08 17:03:45 +0100

    improved form styling for consistency

--- client/src/components/FormItem/index.js
+++ client/src/components/FormItem/index.js
@@ -3,23 +3,25 @@ import "./style.css";
 
 const FormItem = (props) => {
   return (
-    <div className="formItem">
-      <input
-        type="checkbox"
-        id={props.id}
-        name={props.type}
-        value={props.name}
-        checked={props.checked}
-        onChange={props.handleChange}
-        className="checkbox"
-      ></input>
-      <label htmlFor={props.id}> {props.name} </label>
-      <p>
-        Description: <br></br> {props.description}
-      </p>
-      <p>
-        Contact: <a href={`mailto: ${props.email}`}> {props.email}</a>{" "}
-      </p>
+    <div className="form-item-outer">
+      <div className="formItem">
+        <input
+          type="checkbox"
+          id={props.id}
+          name={props.type}
+          value={props.name}
+          checked={props.checked}
+          onChange={props.handleChange}
+          className="checkbox"
+        ></input>
+        <label htmlFor={props.id}> {props.name} </label>
+        <p>
+          Description: <br></br> {props.description}
+        </p>
+        <p>
+          Contact: <a href={`mailto: ${props.email}`}> {props.email}</a>{" "}
+        </p>
+      </div>
     </div>
   );
 };
