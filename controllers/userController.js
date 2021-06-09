46b4e1a72d2a3996c7d73192a98954e551ee3a84
Matthew Jones <mejones92@gmail.com>
2021-06-09 20:38:34 +0100

    Fixed password hashing error during login auth.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,51 +1,56 @@
 const User = require("../models/User.js");
+const bcrypt = require("bcrypt");
 
 module.exports = {
-    findUserById: function(req, res) {
-        User.findById(req.params.id)
-            .then(dbUser => res.json(dbUser))
-            .catch(err => res.status(422).json(err));
-    },
+  findUserById: function(req, res) {
+    User.findById(req.params.id)
+      .then(dbUser => res.json(dbUser))
+      .catch(err => res.status(422).json(err));
+  },
 
-    create: function(req, res) {
-        User.create(req.body)
-            .then(dbUser => res.json(dbUser))
-            .catch(err => res.status(422).json(err));
-    },
+  create: function(req, res) {
+    User.create(req.body)
+      .then(dbUser => res.json(dbUser))
+      .catch(err => res.status(422).json(err));
+  },
 
-    authenticate: function(req, res) {
-        User.findOne({ email: req.body.email, password: req.body.password }, function(err, user) {
-            if (err) {
-                console.log(err);
-                return res.status(500).send();
-            }
-
-            if (!user) {
-                return res.status(404).send('User not found');
-            }
-
-            req.session.user = user;
-            return res.status(200).send('Successfully logged in');
-        });
-
-    },
-
-    session: function(req, res) {
-        if (!req.session.user) {
-            return res.status(401).send();
+  authenticate: function(req, res) {
+    try {
+      User.findOne({ email: req.body.email }, function(error, user) {
+        if(!user) {
+          return res.status(400).send({ message: "The username does not exist" });
         }
 
-        return res.status(200).send(req.session.user);
-    },
+        bcrypt.compare(req.body.password, user.password, (error, match) => {
+          if(match) {
+            req.session.user = user;
+            return res.status(200).send('Successfully logged in');
+          }
+                    
+          return res.status(400).send({ message: "The password is invalid" });
+        });
+      });
+    } catch (error) {
+        res.status(500).send(error);
+    }
+  },
 
-    logout: function(req, res) {
-        req.session.destroy();
-        return res.status(200).send('Successfully ended session');
-    },
+  session: function(req, res) {
+    if (!req.session.user) {
+      return res.status(401).send();
+    }
 
-    update: function(req, res) {
-        User.findOneAndUpdate({ _id: req.params.id }, { password: req.body.password })
-            .then(dbUser => res.json(dbUser))
-            .catch(err => res.status(422).json(err));
-    },
+    return res.status(200).send(req.session.user);
+  },
+
+  logout: function(req, res) {
+    req.session.destroy();
+    return res.status(200).send('Successfully ended session');
+  },
+
+  update: function(req, res) {
+    User.findOneAndUpdate({ _id: req.params.id }, { password: req.body.password })
+      .then(dbUser => res.json(dbUser))
+      .catch(err => res.status(422).json(err));
+  },
 };
