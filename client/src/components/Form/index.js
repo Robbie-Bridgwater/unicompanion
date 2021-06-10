0736112b336dcdb04db3a297ebc943377be35274
Ben <pauleybj@gmail.com>
2021-06-10 14:48:05 +0100

    improved mobile-responsive layout of SnS page

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -162,102 +162,101 @@ export const Form = () => {
       <Container fluid>
         <Row>
           <form onSubmit={handleSubmit}>
-            <Col
-              size="xs-10 sm-10 md-6 lg-6"
-              myclass="sportCol d-flex justify-content-center"
-            >
-              <div>
-                <h4 className="header">Sports Teams</h4>
-                <br></br>
-                <FormItem
-                  key={sports[0].id}
-                  type={sports[0].type}
-                  name={sports[0].name}
-                  checked={isSports.Football}
-                  description={sports[0].description}
-                  email={sports[0].email}
-                  handleChange={handleChange}
-                />
-                <FormItem
-                  key={sports[1].id}
-                  type={sports[1].type}
-                  name={sports[1].name}
-                  checked={isSports.Hockey}
-                  description={sports[1].description}
-                  email={sports[1].email}
-                  handleChange={handleChange}
-                />
-                <FormItem
-                  key={sports[2].id}
-                  type={sports[2].type}
-                  name={sports[2].name}
-                  checked={isSports.Rugby}
-                  description={sports[2].description}
-                  email={sports[2].email}
-                  handleChange={handleChange}
-                />
-                <FormItem
-                  key={sports[3].id}
-                  type={sports[3].type}
-                  name={sports[3].name}
-                  checked={isSports.Lacrosse}
-                  description={sports[3].description}
-                  email={sports[3].email}
-                  handleChange={handleChange}
-                />
-              </div>
-            </Col>
-            <Col
-              size="xs-10 sm-10 md-6 lg-6"
-              myclass="socCol d-flex justify-content-center"
-            >
-              <div>
-                <h4 className="header">Societies</h4>
-                <br></br>
-                <FormItem
-                  key={societies[0].id}
-                  type={societies[0].type}
-                  name={societies[0].name}
-                  checked={isSocieties.Film}
-                  description={societies[0].description}
-                  email={societies[0].email}
-                  handleChange={handleChange}
-                />
-                <FormItem
-                  key={societies[1].id}
-                  type={societies[1].type}
-                  name={societies[1].name}
-                  checked={isSocieties.Dance}
-                  description={societies[1].description}
-                  email={societies[1].email}
-                  handleChange={handleChange}
-                />
-                <FormItem
-                  key={societies[2].id}
-                  type={societies[2].type}
-                  name={societies[2].name}
-                  checked={isSocieties.Music}
-                  description={societies[2].description}
-                  email={societies[2].email}
-                  handleChange={handleChange}
-                />
-                <FormItem
-                  key={societies[3].id}
-                  type={societies[3].type}
-                  name={societies[3].name}
-                  checked={isSocieties.Drinking}
-                  description={societies[3].description}
-                  email={societies[3].email}
-                  handleChange={handleChange}
-                />
-              </div>
-            </Col>
-            <br></br>
             <Row>
               <Col
-                size="xs-12 sm-12 md-12 lg-12"
-                myclass="d-flex justify-content-center"
+                size="xs-10 sm-10 md-6 lg-6"
+                myclass="sportCol d-flex justify-content-center"
               >
+                <div>
+                  <h4 className="header">Sports Teams</h4>
+                  <br></br>
+                  <FormItem
+                    key={sports[0].id}
+                    type={sports[0].type}
+                    name={sports[0].name}
+                    checked={isSports.Football}
+                    description={sports[0].description}
+                    email={sports[0].email}
+                    handleChange={handleChange}
+                  />
+                  <FormItem
+                    key={sports[1].id}
+                    type={sports[1].type}
+                    name={sports[1].name}
+                    checked={isSports.Hockey}
+                    description={sports[1].description}
+                    email={sports[1].email}
+                    handleChange={handleChange}
+                  />
+                  <FormItem
+                    key={sports[2].id}
+                    type={sports[2].type}
+                    name={sports[2].name}
+                    checked={isSports.Rugby}
+                    description={sports[2].description}
+                    email={sports[2].email}
+                    handleChange={handleChange}
+                  />
+                  <FormItem
+                    key={sports[3].id}
+                    type={sports[3].type}
+                    name={sports[3].name}
+                    checked={isSports.Lacrosse}
+                    description={sports[3].description}
+                    email={sports[3].email}
+                    handleChange={handleChange}
+                  />
+                </div>
+              </Col>
+              <Col
+                size="xs-10 sm-10 md-6 lg-6"
+                myclass="socCol d-flex justify-content-center"
+              >
+                <div>
+                  <h4 className="header">Societies</h4>
+                  <br></br>
+                  <FormItem
+                    key={societies[0].id}
+                    type={societies[0].type}
+                    name={societies[0].name}
+                    checked={isSocieties.Film}
+                    description={societies[0].description}
+                    email={societies[0].email}
+                    handleChange={handleChange}
+                  />
+                  <FormItem
+                    key={societies[1].id}
+                    type={societies[1].type}
+                    name={societies[1].name}
+                    checked={isSocieties.Dance}
+                    description={societies[1].description}
+                    email={societies[1].email}
+                    handleChange={handleChange}
+                  />
+                  <FormItem
+                    key={societies[2].id}
+                    type={societies[2].type}
+                    name={societies[2].name}
+                    checked={isSocieties.Music}
+                    description={societies[2].description}
+                    email={societies[2].email}
+                    handleChange={handleChange}
+                  />
+                  <FormItem
+                    key={societies[3].id}
+                    type={societies[3].type}
+                    name={societies[3].name}
+                    checked={isSocieties.Drinking}
+                    description={societies[3].description}
+                    email={societies[3].email}
+                    handleChange={handleChange}
+                  />
+                </div>
+              </Col>
+            </Row>
+            <br></br>
+            <Row>
+              <Col size="12" myclass="d-flex justify-content-center">
                 <div className="form-group">
                   <button type="submit" className="sns-submit-btn">
                     SUBMIT
