add187622727496bb650206fa2d903952f660fef
Ben <pauleybj@gmail.com>
2021-06-11 21:02:41 +0100

    made code my dry

--- client/src/components/Sidebar/index.js
+++ client/src/components/Sidebar/index.js
@@ -21,123 +21,78 @@ const Sidebar = () => {
     });
   }, []);
 
-  if (loggedIn) {
-    return (
-      <div
-        style={{
-          display: "flex",
-          height: "100vh",
-          overflow: "scroll initial",
-          zIndex: 10,
-          float: "left",
-          position: "sticky",
-          top: 0,
-        }}
-      >
-        <CDBSidebar textColor="#fff" backgroundColor="#333">
-          <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
-            <a
-              href="/"
-              className="text-decoration-none"
-              style={{ color: "inherit" }}
-            >
-              University
-            </a>
-          </CDBSidebarHeader>
+  return (
+    <div
+      style={{
+        display: "flex",
+        height: "100vh",
+        overflow: "scroll initial",
+        zIndex: 10,
+        float: "left",
+        position: "sticky",
+        top: 0,
+      }}
+    >
+      <CDBSidebar textColor="#fff" backgroundColor="#333">
+        <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
+          <a
+            href="/"
+            className="text-decoration-none"
+            style={{ color: "inherit" }}
+          >
+            University
+          </a>
+        </CDBSidebarHeader>
 
-          <CDBSidebarContent className="sidebar-content">
-            <CDBSidebarMenu>
-              <NavLink exact to="/" activeClassName="activeClicked">
-                <CDBSidebarMenuItem icon="home">Home</CDBSidebarMenuItem>
-              </NavLink>
-              <NavLink exact to="/calendar" activeClassName="activeClicked">
-                <CDBSidebarMenuItem icon="calendar">
-                  Calendar
-                </CDBSidebarMenuItem>
-              </NavLink>
-              <NavLink
-                exact
-                to="/sportsandsocieties"
-                activeClassName="activeClicked"
-              >
-                <CDBSidebarMenuItem icon="running">
-                  Sports and Societies
-                </CDBSidebarMenuItem>
-              </NavLink>
-              <NavLink exact to="/account" activeClassName="activeClicked">
-                <CDBSidebarMenuItem icon="user">Account</CDBSidebarMenuItem>
-              </NavLink>
-              <NavLink exact to="/about" activeClassName="activeClicked">
-                <CDBSidebarMenuItem icon="question">About</CDBSidebarMenuItem>
-              </NavLink>
-            </CDBSidebarMenu>
-          </CDBSidebarContent>
+        <CDBSidebarContent className="sidebar-content">
+          <CDBSidebarMenu>
+            <NavLink exact to="/" activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="home">Home</CDBSidebarMenuItem>
+            </NavLink>
 
-          <CDBSidebarFooter style={{ textAlign: "center" }}>
-            <div
-              style={{
-                padding: "20px 5px",
-                fontSize: "13px",
-              }}
-            >
-              © Uni<br></br>Companion
-            </div>
-          </CDBSidebarFooter>
-        </CDBSidebar>
-      </div>
-    );
-  } else {
-    return (
-      <div
-        style={{
-          display: "flex",
-          height: "100vh",
-          overflow: "scroll initial",
-          zIndex: 10,
-          float: "left",
-          position: "sticky",
-          top: 0,
-        }}
-      >
-        <CDBSidebar textColor="#fff" backgroundColor="#333">
-          <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
-            <a
-              href="/"
-              className="text-decoration-none"
-              style={{ color: "inherit" }}
-            >
-              University
-            </a>
-          </CDBSidebarHeader>
+            {loggedIn ? (
+              <>
+                <NavLink exact to="/calendar" activeClassName="activeClicked">
+                  <CDBSidebarMenuItem icon="calendar">
+                    Calendar
+                  </CDBSidebarMenuItem>
+                </NavLink>
+                <NavLink
+                  exact
+                  to="/sportsandsocieties"
+                  activeClassName="activeClicked"
+                >
+                  <CDBSidebarMenuItem icon="running">
+                    Sports and Societies
+                  </CDBSidebarMenuItem>
+                </NavLink>
+              </>
+            ) : (
+              <></>
+            )}
 
-          <CDBSidebarContent className="sidebar-content">
-            <CDBSidebarMenu>
-              <NavLink exact to="/" activeClassName="activeClicked">
-                <CDBSidebarMenuItem icon="home">Home</CDBSidebarMenuItem>
-              </NavLink>
-              <NavLink exact to="/account" activeClassName="activeClicked">
-                <CDBSidebarMenuItem icon="user">Account</CDBSidebarMenuItem>
-              </NavLink>
-              <NavLink exact to="/about" activeClassName="activeClicked">
-                <CDBSidebarMenuItem icon="question">About</CDBSidebarMenuItem>
-              </NavLink>
-            </CDBSidebarMenu>
-          </CDBSidebarContent>
+            <NavLink exact to="/account" activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="user">Account</CDBSidebarMenuItem>
+            </NavLink>
+            <NavLink exact to="/about" activeClassName="activeClicked">
+              <CDBSidebarMenuItem icon="question">About</CDBSidebarMenuItem>
+            </NavLink>
+          </CDBSidebarMenu>
+        </CDBSidebarContent>
 
-          <CDBSidebarFooter style={{ textAlign: "center" }}>
-            <div
-              style={{
-                padding: "20px 5px",
-                fontSize: "13px",
-              }}
-            >
-              © Uni<br></br>Companion
-            </div>
-          </CDBSidebarFooter>
-        </CDBSidebar>
-      </div>
-    );
-  }
+        <CDBSidebarFooter style={{ textAlign: "center" }}>
+          <div
+            style={{
+              padding: "20px 5px",
+              fontSize: "13px",
+            }}
+          >
+            © Uni<br></br>Companion
+          </div>
+        </CDBSidebarFooter>
+      </CDBSidebar>
+    </div>
+  );
 };
 
 export default Sidebar;
